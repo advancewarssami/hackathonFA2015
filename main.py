5ade1e8200573ba2c2f930142a99a3151df3b096
Jordan <jomiander3@gmail.com>
2015-11-14 15:27:05 -0600

    File Hierarchy and Input Start

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
     return render_template('index.html')
 
-@app.route("/home")
-def home():
-    return "Home"
+
+@app.route("/create_group")
+def create_group():
+    return "Create Group"
+
+
+@app.route("/group")
+def group():
+    return render_template('group.html')
+
+
+@app.route("/start")
+def start():
+    return render_template('start_page.html')
+
+
+@app.route('/input')
+def input_page():
+    return render_template('input.html')
+
 
 if __name__ == "__main__":
     app.run()
